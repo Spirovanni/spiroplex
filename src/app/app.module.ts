ea8a3ec5a23dc57a97d2ddb34e7db35aefbfe347
Spirovanni <x@ampdin.com>
2017-08-01 23:47:56 -0700

    Third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { NgsRevealModule } from 'ng-scrollreveal';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
@@ -12,6 +13,14 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NAV_DROPDOWN_DIRECTIVES } from './shared/nav-dropdown.directive';
+
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { SIDEBAR_TOGGLE_DIRECTIVES } from './shared/sidebar.directive';
+import { AsideToggleDirective } from './shared/aside.directive';
+import { BreadcrumbsComponent } from './shared/breadcrumb.component';
 
 // Routing Module
 import { AppRoutingModule } from './app.routing';
@@ -24,12 +33,20 @@ import { SimpleLayoutComponent } from './layouts/simple-layout.component';
   declarations: [
     AppComponent,
     FullLayoutComponent,
-    SimpleLayoutComponent
+    SimpleLayoutComponent,
+    NAV_DROPDOWN_DIRECTIVES,
+    BreadcrumbsComponent,
+    SIDEBAR_TOGGLE_DIRECTIVES,
+    AsideToggleDirective
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    NgsRevealModule.forRoot(),
+    ChartsModule,
     FormsModule,
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase, 'spiroplex'),
--- src/app/views/splash/splash.module.ts
+++ src/app/views/splash/splash.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 
 import { SplashComponent } from './splash.component';
 import { SplashRoutingModule } from './splash-routing.module';
 
 @NgModule({
-    imports: [
-      SplashRoutingModule
-    ],
-    declarations: [ SplashComponent ]
+  imports: [
+    SplashRoutingModule,
+    CarouselModule.forRoot(),
+    ChartsModule
+  ],
+  declarations: [ SplashComponent ]
 })
 export class SplashModule { }
